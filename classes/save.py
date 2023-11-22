7691938123b7c3947574561237ff8062e1138e40
FL-Lion <richard.nied2@gmail.com>
2023-11-22 16:03:43 +0100

    finished SaveManager class

--- classes/save.py
+++ classes/save.py
@@ -1,27 +1,45 @@
 import json
+from cryptography.fernet import Fernet
 
 class SaveManager():
-    def __init__(self,path,key) -> None:
+    def __init__(self,path="data.save",key=b"z8IwBgA-gFs66DrrM7JHtXe0fl9OVtL3A8Q-xU1nmAA=") -> None:
         self.path = path
         self.key = key
     
     def encrypt(self) -> bytes:
-        pass
+        with open(self.path, 'rb') as file:
+            data = file.read()
+
+        fernet = Fernet(self.key)
+        encrypted_data = fernet.encrypt(data)
+
+        with open(self.path, 'wb') as file:
+            file.write(encrypted_data)
     
     def decrypt(self) -> bytes:
-        pass
-    
-    def save(self,value,key):
-        self.decrypt()
         try:
             with open(self.path, 'rb') as file:
+                data = file.read()
+            if data != b'':
+                fernet = Fernet(self.key)
+                decrypted_data = fernet.decrypt(data)
+
+                with open(self.path, 'wb') as file:
+                    file.write(decrypted_data)
+        except FileNotFoundError:
+            pass
+    
+    def save(self,key,value):
+        self.decrypt()
+        try:
+            with open(self.path, 'r') as file:
                 data = json.load(file)
         except FileNotFoundError:
             data = {}
 
         data[key] = value
 
-        with open(self.path, 'wb') as file:
+        with open(self.path, 'w') as file:
             json.dump(data, file, indent=4)
         self.encrypt()
     
@@ -39,6 +57,3 @@ class SaveManager():
         except (FileNotFoundError, json.JSONDecodeError):
             self.encrypt()
             return None
-        
-x = SaveManager('test.save',b"4XLvJ06F6OriXAYLUUjBzkS--AISlUp8P1V2b4QzKA0=")
-print(x.load('hu'))
