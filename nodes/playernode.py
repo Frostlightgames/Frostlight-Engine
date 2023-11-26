ea9427591e602866dde26213a081d0e404553f6f
FL-Lion <richard.nied2@gmail.com>
2023-11-26 14:59:27 +0100

    added double jump to PlayerNodePlatformer

--- nodes/playernode.py
+++ nodes/playernode.py
@@ -36,19 +36,24 @@ class PlayerNodeRPG(PlayerNode):
         super().draw()
 
 class PlayerNodePlatformer(PlayerNode):
-    def __init__(self, engine, sprite: pygame.Surface, jump_input:str, acc: int = 200, dcc: int = 350, gravity:int=300, ground:int=500, max_speed: int = 1000, max_jump_height: int = 100, jump_strength: int = 200, coyote_time_duration: float = 0.2) -> None:
+    def __init__(self, engine, sprite: pygame.Surface, jump_input:str, acc: int = 200, dcc: int = 350, gravity:int=300, ground:int=500, max_speed: int = 1000, max_jump_height: int = 100, jump_strength: int = 200, coyote_time_duration: float = 0.2, jump_count: int = 1) -> None:
         super().__init__(engine, sprite, acc, dcc, max_speed)
         self.ground = ground
         self.on_ground = True
         self.gravity = gravity
         self.jump_strength = jump_strength
         self.is_jumping = False
+        self.jumping_phase = False
         self.coyote_time = 0.0
         self.coyote_time_duration = coyote_time_duration
         self.jump_pressed = False
         self.max_jump_height = max_jump_height
         self.remaining_jump_height = max_jump_height
         self.jump_input = jump_input
+        self.jump_buffer = False
+        self.prev_jump_input = False
+        self.jump_count = jump_count
+        self.available_jumps = jump_count
 
     def update(self):
         self.vel.x += (self.engine.input.get("right")-self.engine.input.get("left"))*self.acc*self.engine.delta_time
@@ -64,22 +69,35 @@ class PlayerNodePlatformer(PlayerNode):
         self.coyote_time = (self.coyote_time + self.engine.delta_time) * int(not self.on_ground)
 
         if self.engine.input.get(self.jump_input):
-            if (self.on_ground or self.coyote_time < self.coyote_time_duration) and not self.is_jumping and self.remaining_jump_height > 0:
+            if (self.on_ground or self.coyote_time < self.coyote_time_duration or self.available_jumps > 0) and not self.is_jumping and (not self.jumping_phase or self.available_jumps > 0):
                 self.is_jumping = True
+                self.jumping_phase = True
                 self.jump_pressed = True
+                self.available_jumps -= 1
+                self.remaining_jump_height = self.max_jump_height
+
+        if not self.on_ground and self.engine.input.get(self.jump_input) and not self.prev_jump_input and not self.coyote_time < self.coyote_time_duration and self.remaining_jump_height > 0 and self.available_jumps == 0:
+            self.jump_buffer = True
+
+        if (self.on_ground or self.coyote_time < self.coyote_time_duration) and self.jump_buffer and not self.is_jumping and self.remaining_jump_height > 0 and not self.jumping_phase:
+            self.is_jumping = True
+            self.jump_buffer = False
+            self.jumping_phase = True
+            self.jump_pressed = True
+
+        self.prev_jump_input = self.engine.input.get(self.jump_input)
 
         if self.is_jumping:
             self.vel.y = -self.jump_strength
             self.remaining_jump_height -= abs(self.vel.y) * self.engine.delta_time  # Reduziere die verbleibende Sprunghöhe
             if not self.jump_pressed or self.remaining_jump_height <= 0:
                 self.is_jumping = False
-        
+
         #if not self.engine.input.get("jump"):
         #    self.jump_pressed = False
 
         self.jump_pressed = int(self.jump_pressed) * int(self.engine.input.get(self.jump_input))
         
-        # Gravity
         self.vel.y += self.gravity * self.engine.delta_time
         self.pos.y += self.vel.y * self.engine.delta_time
 
@@ -95,8 +113,10 @@ class PlayerNodePlatformer(PlayerNode):
 
         collision_ground = self.pos.y + self.sprite.get_height() >= self.ground
 
+        self.available_jumps = max(self.jump_count*int(collision_ground),self.available_jumps*int(not collision_ground))
         self.pos.y = max((self.ground - self.sprite.get_height())*collision_ground,self.pos.y*int(not collision_ground))
         self.vel.y = int(not collision_ground)*self.vel.y
         self.on_ground = max(collision_ground,self.on_ground)
         self.is_jumping = int(not collision_ground)*int(self.is_jumping)
         self.remaining_jump_height = max(self.max_jump_height*collision_ground,self.remaining_jump_height*int(not collision_ground))
+        self.jumping_phase = int(not collision_ground)*int(self.jumping_phase)
