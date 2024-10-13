6f6be13213ac2c82e961a4506795fc2395fe39ae
gamexdifficulty <gamexdifficulty@yahoo.com>
2024-10-14 01:54:42 +0200

    Added fps clock

--- _core/core.py
+++ _core/core.py
@@ -8,6 +8,7 @@ from _core.save_manager import SaveManager
 from _core.window import Window
 class Core:
     def __init__(self,debug:bool=False) -> None:
+        self.clock = pygame.time.Clock()
 
         self.logger = Logger(logging=ENV.values["logging"],logging_only_once=ENV.values["logging_only_once"])
         self.save_manager = SaveManager(logger=self.logger,path=ENV.values["save_manager_path"])
@@ -19,6 +20,7 @@ class Core:
 
     def loop(self,*functions):
         while True:
+            self.clock.tick(ENV.values["fps_limit"])
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     exit(0)
