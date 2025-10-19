a7b1a9052a29da219e6aa6c2b9bbe964b86c5115
gamexdifficulty <gamexdifficulty@yahoo.com>
2025-10-19 03:01:22 +0200

    Re worked sprite class to account for custom shaders

--- core/sprite.py
+++ core/sprite.py
@@ -1,194 +1,129 @@
 from init import *
-import init as init
+from utils.load_shader import load_shader
 
 class Sprite:
-    """
-    A 2D textured sprite.
-
-    This sprite uses a textured quad rendered via OpenGL shaders,
-    allowing positioning, scaling, and rotation in screen space.
-
-    Attributes:
-        ctx: The ModernGL context (retrieved from global WINDOW_CONTEXT).
-        texture: The loaded OpenGL texture.
-        program: The ModernGL shader program used for rendering.
-        vbo: Vertex Buffer Object containing quad vertex data.
-        vao: Vertex Array Object used to bind shaders and buffers for drawing.
-        width: Width of the loaded texture in pixels.
-        height: Height of the loaded texture in pixels.
-    """
-
-    def __init__(self, image_path, size:list=None):
-        """
-        Initializes the Sprite by loading the texture, compiling shaders,
-        and setting up buffers for rendering.
-
-        Args:
-            image_path (str): Path to the image file used as the sprite's texture.
-        """
-
-        self.__ctx = init.WINDOW_CONTEXT
-        self.__size = [1.0,1.0]
-        self._texture = self.__load_texture(image_path)
-        self.__program = self.__create_program()
-        self.__vbo = self.__create_quad()
-        self._vao = self.__ctx.vertex_array(self.__program,[(self.__vbo, '2f 2f', 'in_vert', 'in_tex')])
-        self.__alpha = 1.0
-        self.__rotation = 0
-        self.__pivot_point = [0.0,0.0]
+    def __init__(self, image_path="", size:list=None, texture_filter=moderngl.NEAREST, direct_image_path=None):
+        self.alpha = 1.0
+        self.flipped = False
+        self.pivot_point = [0,0]
+        self.rotation = 0
+        self.size = [1,1]
 
         if size != None:
-            self.set_size(size)
+            self.size = size
 
-    def set_alpha(self, alpha:float=1.0):
-        if alpha > 1.0 or alpha < 0.0:
+        if direct_image_path == None:
+            image_path = os.path.join("data","sprites",image_path)
+        else:
+            image_path = direct_image_path
+
+        self._ctx:moderngl.Context = GLOBAL_ENVIRONMENT.MODERNGL_CONTEXT
+        self._texture = self._load_texture(image_path,texture_filter,size)
+        self._program = self._ctx.program(load_shader("default.vert"),load_shader("default.frag"))
+        self._vbo = self._ctx.buffer(numpy.array([-0.5, 0.5, 0.0, 0.0, 0.5, 0.5, 1.0, 0.0, -0.5, -0.5, 0.0, 1.0, 0.5, -0.5, 1.0, 1.0,],"f4"))
+        self._vao = self._ctx.vertex_array(self._program,[(self._vbo,"2f 2f", "vert", "tex")])
+        self._custom_program: moderngl.Program = None
+
+    @property
+    def alpha(self):
+        return self._alpha
+    
+    @alpha.setter
+    def alpha(self, value:float=1.0):
+        if type(value) not in [int,float] or value > 1.0 or value < 0.0:
             raise ValueError("Alpha value must be between 0.0 and 1.0")
         
-        self.__alpha = alpha
+        self._alpha = value
 
-    def get_alpha(self):
-        return self.__alpha
-
-    def set_rotation(self, rotation:float=0.0):
-        self.__rotation = rotation
-
-    def get_rotation(self):
-        return self.__rotation
+    @property
+    def flipped(self):
+        return self._flipped
     
-    def set_size(self, size:list=[1.0,1.0]):
-        if size[0] < 0 or size[1] < 0:
-            raise ValueError("Any size parameter must be larger than 0")
+    @flipped.setter
+    def flipped(self, value:int|bool=False):
+        if type(value) not in [bool,int] or value not in [0,1]:
+            raise ValueError("Flipped must be a boolean, 0 or 1")
         
-        self.__size = size
+        self._flipped = value
 
-    def get_size(self):
-        return self.__size
+    @property
+    def pivot_point(self):
+        return self._pivot_point
     
-    def scale_by(self, factor:float=1.0):
+    @pivot_point.setter
+    def pivot_point(self, value:list[float]=[0.0,0.0]):
+        if type(value) != list or type(value[0]) not in [int,float] or type(value[1]) not in [int,float] or len(value) != 2:
+            raise ValueError("Pivot Point must be a list with 2 floats")
+        
+        self._pivot_point = value
+
+    @property
+    def rotation(self):
+        return self._rotation
+    
+    @rotation.setter
+    def rotation(self, value:int|int=0):
+        if type(value) not in [int,float]:
+            raise ValueError("Rotation must be a integer or float")
+        
+        self._rotation = value
+
+    @property
+    def size(self):
+        return self._size
+    
+    @size.setter
+    def size(self, value:list[int]=[1,1]):
+        if type(value) != list or type(value[0]) != int or type(value[1]) != int or len(value) != 2 or value[0] < 0 or value[1] < 0:
+            raise ValueError("Size must be a list with 2 positive integer")
+        
+        self._size = value
+
+    def scale_by(self,factor:float=2.0):
         if factor < 0.0:
             raise ValueError("Sizing factor must be larger than 0")
         
-        self.set_size([self.__size[0]*factor,self.__size[1]*factor])
+        self.size = [int(self.size[0]*factor),int(self.size[1]*factor)]
 
-    def set_pivot_point(self, pivot_point:list=[0.0,0.0]):
-        self.__pivot_point = pivot_point
+    def set_custom_shader(self, fragment_shader:str):
+        self._program.release()
+        self._vao.release()
+        self._program = self._ctx.program(load_shader("default.vert"),fragment_shader)
+        self._vao = self._ctx.vertex_array(self._program,[(self._vbo,"2f 2f", "vert", "tex")])
 
-    def get_pivot_point(self):
-        return self.__pivot_point
-
-    def __create_program(self):
-        """
-        Compiles and links the vertex and fragment shaders into a ModernGL program.
-
-        Returns:
-            moderngl.Program: The compiled shader program.
-        """
-         
-        return self.__ctx.program(
-            vertex_shader="""
-                #version 330
-                uniform vec2 offset;
-                uniform vec2 scale_pixels;
-                uniform vec2 screen_size;
-                uniform float rotation;
-                uniform float alpha;
-                uniform vec2 pivot;
-
-                in vec2 in_vert;
-                in vec2 in_tex;
-                out vec2 v_tex;
-
-                void main() {
-                    float c = cos(rotation);
-                    float s = sin(rotation);
-
-                    vec2 local = in_vert - pivot;
-                    vec2 scaled = local * scale_pixels;
-                    vec2 rotated = vec2(
-                        scaled.x * c - scaled.y * s,
-                        scaled.x * s + scaled.y * c
-                    );
-
-                    vec2 transformed = rotated + pivot * scale_pixels;
-                    vec2 ndc = vec2(
-                        transformed.x * 2.0 / screen_size.x,
-                        transformed.y * 2.0 / screen_size.y
-                    );
-
-                    gl_Position = vec4(offset + ndc, 0.0, 1.0);
-                    v_tex = in_tex;
-                }
-            """,
-            fragment_shader="""
-                #version 330
-                uniform sampler2D Texture;
-                uniform float alpha;
-                in vec2 v_tex;
-                out vec4 fragColor;
-
-                void main() {
-                    vec4 texColor = texture(Texture, v_tex);
-                    fragColor = vec4(texColor.rgb, texColor.a * alpha);
-                }
-            """
-        )
-
-    def __create_quad(self):
-        """
-        Creates a quad with position and texture coordinates.
-
-        The quad is centered at (0,0) and spans from -0.5 to 0.5.
-
-        Returns:
-            moderngl.Buffer: Vertex buffer for the quad.
-        """
-
-        vertices = numpy.array([
-            -0.5, -0.5, 0.0, 1.0,
-             0.5, -0.5, 1.0, 1.0,
-            -0.5,  0.5, 0.0, 0.0,
-             0.5,  0.5, 1.0, 0.0,
-        ], dtype='f4')
-        return self.__ctx.buffer(vertices.tobytes())
-
-    def __load_texture(self, path):
-        """
-        Loads an image and creates an OpenGL texture.
-
-        Args:
-            path (str): File path to the image.
-
-        Returns:
-            moderngl.Texture: The OpenGL texture object.
-        """
-
+    def _load_texture(self,path,texture_filter,preferred_size):
         img = Image.open(path).convert('RGBA')
-        self.__width, self.__height = img.size
-        self.__size = [self.__width,self.__height]
-        texture = self.__ctx.texture((self.__width, self.__height), 4, img.tobytes())
+        self.size = [img.size[0],img.size[1]]
+        
+        texture = self._ctx.texture(self.size, 4, img.tobytes())
+        texture.filter = (texture_filter, moderngl.NEAREST)
 
-        texture.filter = (moderngl.NEAREST, moderngl.NEAREST)
+        if preferred_size != None:
+            self.size = preferred_size
 
-        texture.use()
         return texture
     
-    def _set_uniforms(self, pos, screen_size, centered):
+    def _render(self, pos, canvas_size, centered):
         if centered:
-            normal_x = (pos[0] / screen_size[0]) * 2.0 - 1.0
-            normal_y = 1.0 - (pos[1] / screen_size[1]) * 2.0
+            normal_x = (pos[0] / canvas_size[0]) * 2.0 - 1.0
+            normal_y = 1.0 - (pos[1] / canvas_size[1]) * 2.0
         else:
-            normal_x = ((pos[0] + self.__size[0] / 2.0) / screen_size[0]) * 2.0 - 1.0
-            normal_y = 1.0 - ((pos[1] + self.__size[1] / 2.0) / screen_size[1]) * 2.0
+            normal_x = ((pos[0] + self.size[0] / 2.0) / canvas_size[0]) * 2.0 - 1.0
+            normal_y = 1.0 - ((pos[1] + self.size[1] / 2.0) / canvas_size[1]) * 2.0
 
-        size_x = float(self.__size[0])
-        size_y = float(self.__size[1])
+        size_x = float(self.size[0])
+        size_y = float(self.size[1])
 
-        self.__program['offset'].value = (normal_x, normal_y)
-        self.__program['scale_pixels'].value = (size_x, size_y)
-        self.__program['screen_size'].value = (float(screen_size[0]), float(screen_size[1]))
-        self.__program['rotation'].value = math.radians(-self.__rotation)
-        self.__program['alpha'].value = self.__alpha
-        self.__program['pivot'].value = self.__pivot_point
+        if "offset" in self._program:
+            self._program["offset"].value = (normal_x, normal_y)
+        if "scale_pixels" in self._program:
+            self._program["scale_pixels"].value = (size_x, size_y)
+        if "screen_size" in self._program:
+            self._program["screen_size"].value = (float(canvas_size[0]), float(canvas_size[1]))
+        if "rotation" in self._program:
+            self._program["rotation"].value = math.radians(-self.rotation)
+        if "alpha" in self._program:
+            self._program["alpha"].value = self.alpha
+        
         self._texture.use()
-
-
+        self._vao.render(moderngl.TRIANGLE_STRIP)
