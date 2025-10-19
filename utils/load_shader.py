9d4f08bcf39cad3caf3a6209a4ff7b6ffe7102fa
gamexdifficulty <gamexdifficulty@yahoo.com>
2025-10-19 02:56:50 +0200

    Added load shader util tool

---
+++ utils/load_shader.py
@@ -0,0 +1,12 @@
+import os
+
+def load_shader(path:str="",direct_path=None):
+    if direct_path == None:
+        path = os.path.join("data","shaders",path)
+    else:
+        path = direct_path
+
+    with open(path,"r") as f:
+        data = f.read()
+        f.close()
+        return data
